7b7a258d62edf643e5dec9d77c275371925d6234
reset <reset@javiers-MacBook-Air.local>
2024-01-19 21:53:31 -0600

    finish clone

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import styled from "styled-components"
-// import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
 
 
 export const Header = () => {
+  const [hamburgerState,setHamburgerState] = useState(false);
   return (
     <Container>
       <a>
@@ -17,7 +21,20 @@ export const Header = () => {
       <RaightMenu>
         <a href="#">Shop</a>
         <a href="#">tesla account</a>
+        <CustomMenu onClick={()=> setHamburgerState(true)}/>
       </RaightMenu>
+      <BurgerNav show={hamburgerState}>
+        <CloseMenuContainer>
+          <CloseMenu onClick={()=> setHamburgerState(false)}/>
+        </CloseMenuContainer>
+        <li><a href="#">Existing Inventory</a></li>
+        <li><a href="#">Used Inventory</a></li>
+        <li><a href="#">Trade-in</a></li>
+        <li><a href="#">Cybertruck</a></li>
+        <li><a href="#">Existing Inventory</a></li>
+        <li><a href="#">Existing Inventory</a></li>
+        <li><a href="#">Existing Inventory</a></li>
+      </BurgerNav>
     </Container>
   )
 }
@@ -26,10 +43,12 @@ const Container = styled.div`
   position:fixed;
   display:flex;
   align-items:center;
+  justify-content:space-between;
   padding: 0 20px;
   top:0;
   left:0;
   right:0;
+  z-index:1;
 `
 const Menu = styled.div`
   display:flex;
@@ -41,13 +60,49 @@ const Menu = styled.div`
     text-transform:uppercase;
     padding:0 10px;
     flex-wrap:nowrap;
+    display:block;
+    white-space:nowrap;
+  }
+  @media(max-width:768px){
+    display:none;
   }
 `
 const RaightMenu = styled.div`
+    display:flex;
+    align-items:center;
     a{
     font-weight:bold;
     text-transform:uppercase;
-    margin-right:
+    margin-right:15px;
     flex-wrap:nowrap;
   }
+`
+const CustomMenu = styled(MenuIcon)`
+  cursor:pointer;
+`
+const BurgerNav = styled.div`
+  width:250px;
+  position:fixed;
+  z-index:1000;
+  top:0;
+  right:0;
+  bottom:0;
+  background:white;
+  padding:10px;
+  list-style:none;
+  text-aling:right;
+  transform:${props=>props.show?'translateX(0)':'translateX(100%)'};
+  transition:transform .3s;
+  li{
+    padding:15px 10px;
+    border-bottom: 1px solid #d4d4d4;
+  }
+`
+const CloseMenuContainer = styled.div`
+  display:flex;
+  justify-content:end;
+`
+const CloseMenu = styled(CloseIcon)`
+  margin-bottom: 5px;
+  cursor:pointer;
 `
